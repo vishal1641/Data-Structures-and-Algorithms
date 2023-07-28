76fd8ac56683838f27db8e0ef8afd8291e5a1b7c
vishal1641 <114956239+vishal1641@users.noreply.github.com>
2023-07-29 00:43:20 +0530

    Time: 3 ms (95.73%), Space: 10 MB (8.05%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.cpp
+++ 1168-duplicate-zeros/1168-duplicate-zeros.cpp
@@ -1,41 +1,32 @@
-// class Solution {
-// public:
-//     void duplicateZeros(vector<int>& nums) {
-//     vector<int>v;
-//     int a=0;
-//     for(int i=0;a!=nums.size();i++){
-//         if(nums[i]!=0){
-//   v[a]=nums[i];a++;}
-//         else{
-//             v[a]=0; a++;
-//             v[a]=0;a++;
-//         }
-//     }
-//     nums=v;
-//     }
-// };
 class Solution {
 public:
-    void duplicateZeros(vector<int>& arr) {
-        vector<int>v(arr.begin(),arr.end());
-        int i=0;
-        for(int j=0;j<v.size()&&i<v.size();j++)
+    void duplicateZeros(vector<int>& nums) {
+        vector<int>y=nums;
+        if(count(nums.begin(),nums.end(),0)!=nums.size())
+{
+    vector<int>v;
+   
+    for(int i=0;i<nums.size();i++)
+    {
+
+        if(nums[i]!=0)
         {
-            if(v[j]==0)
-            {
-                arr[i]=0;
-                if(i+1<v.size())
-                {
-                    arr[i+1]=0;
-                }
-                
-                i+=2;
-            }
-            else
-            {
-                arr[i]=v[j];
-                i++;
-            }
+  v.push_back(nums[i]);
+  }
+        else
+        {
+            v.push_back(0);
+           
+            v.push_back(0);
+            nums.pop_back();
+
         }
+      
+       
+    }
+    if(v.size()-1==y.size())
+    v.pop_back();
+   
+    nums=v;}
     }
 };
