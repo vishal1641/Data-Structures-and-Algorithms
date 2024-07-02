84c24c20c47e208ec242a0f7c3c51ec0789a11c3
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-07-02 19:35:42 +0530

    Time: 6 ms (56.76%), Space: 26 MB (43.24%) - LeetHub

---
+++ 3045-minimum-right-shifts-to-sort-the-array/3045-minimum-right-shifts-to-sort-the-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumRightShifts(vector<int>& nums) {
+        vector<int> v;
+        v = nums;
+        sort(begin(v), end(v));
+        int maxi = INT_MIN, mini = INT_MAX, minp = 0, maxp = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (maxi < nums[i]) {
+                maxi = nums[i];
+                maxp = i;
+            }
+            if (mini > nums[i]) {
+                mini = nums[i];
+                minp = i;
+            }
+        }
+        int x = nums.size() - maxp - 1;
+        rotate(nums.begin(), nums.begin() + nums.size() - x, nums.end());
+        if(nums==v&&x==0)
+        return 0;
+        else if(nums==v&&x!=0)
+        return nums.size() - maxp - 1;
+        return -1;
+    }
+};
