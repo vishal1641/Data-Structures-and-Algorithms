fa9abfa899ca0710e5fcc64b1fd4f72cc6a40077
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-08-13 16:22:48 +0530

    Time: 0 ms (100%), Space: 9.6 MB (27.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-int robo(vector<int>&nums,int n,vector<int>&dp){
-    if(n==0)
-    return nums[0];
-    if(n<0)
-    return 0;
-    if(dp[n]!=-1)
-    return dp[n];
-    int sum1=nums[n]+robo(nums,n-2,dp);
-    int sum2=robo(nums,n-1,dp);
-    return dp[n]= max(sum1,sum2);
-}
+    int solve(vector<int>& nums, int i, vector<int>& dp) {
+        if (i >= nums.size())
+            return 0;
+             if (dp[i] != -1)
+            return dp[i];
+        int take = nums[i] + solve(nums,i + 2,dp);
+        int notake = solve(nums,i + 1,dp);
+
+        return dp[i] = max(take, notake);
+    }
     int rob(vector<int>& nums) {
-        vector<int>dp(400,-1);
-       int ans=robo(nums,nums.size()-1,dp);
-       return ans;
+        vector<int> dp(nums.size() + 1, -1);
+        int ans = solve(nums, 0, dp);
+        return ans;
     }
 };
