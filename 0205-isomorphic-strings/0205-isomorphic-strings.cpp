7cafcef6709866e718fb1cf8479d38722bf3a5f0
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-08-29 17:01:47 +0530

    Time: 4 ms (75.95%), Space: 9.7 MB (31.57%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) 
-    {
-        if(s.length()!=t.length())
+    bool isIsomorphic(string s, string b) {
+
+        unordered_map<char, char> mp;
+        bool check = true;
+        set<char> st;
+
+        for (int j = 0; j < b.size(); j++) {
+            if (mp.find(b[j]) != mp.end()) {
+                if (mp[b[j]] != s[j]) {
+                    check = false;
+                    break;
+                }
+            } else {
+                if (st.find(s[j]) != st.end()) {
+                    check = false;
+                    break;
+                }
+                mp[b[j]] = s[j];
+                st.insert(s[j]);
+            }
+        }
+
+        if (check == true)
+            return true;
         return false;
-        map<char,char>m;
-        for(int i=0;i<s.size();i++)
-        {
-
-            if(m.find(s[i])==m.end())
-            m[s[i]]=t[i];
-            else
-            {
-                if(m[s[i]]!=t[i])
-                return false;
-            }
-
-        }
-        map<char,char>l;
-         for(int i=0;i<s.size();i++)
-        {
-
-            if(l.find(t[i])==l.end())
-            l[t[i]]=s[i];
-            else
-            {
-                if(l[t[i]]!=s[i])
-                return false;
-            }
-
-        }
-        return true;
-
-
-        
     }
 };
