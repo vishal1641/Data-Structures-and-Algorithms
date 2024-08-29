f6423c9c1451c79beaee5bbf4cad30c70012c797
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-08-29 23:48:10 +0530

    Time: 43 ms (13.9%), Space: 19.8 MB (9.49%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        map<char, int> m;
-        map<char, int> m1;
-        for (int i = 0; i < p.size(); i++)
-            m1[p[i]]++;
         int i = 0, j = 0;
-vector<int>v;
+        map<char, int> m;
+        for (int i = 0; i < p.size(); i++)
+            m[p[i]]++;
+            int x=0;
+            for(auto it :m){
+                x+=it.second;
+            }
+        map<char, int> m1;
+        vector<int> v;
         while (j < s.size()) {
-            m[s[j]]++;
-            if (j - i + 1 < p.size()) {
+            m1[s[j]]++;
+            if (x > j-i+1) {
                 j++;
-            } else if (j - i + 1 == p.size()) 
-            {
-                if (m == m1)
+            } else if (x == j-i+1) {
+                if (m1 == m)
                     v.push_back(i);
-                m[s[i]]--;
-                if (m[s[i]] == 0)
-                    m.erase(s[i]);
+                m1[s[i]]--;
+                if (m1[s[i]] == 0)
+                    m1.erase(s[i]);
                 i++;
                 j++;
             }
