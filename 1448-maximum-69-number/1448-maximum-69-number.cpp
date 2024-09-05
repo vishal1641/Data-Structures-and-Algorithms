648dd4679253aeba2de35ee85482cdcd69ebd79f
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-05 13:43:13 +0530

    Time: 3 ms (41.96%), Space: 7.8 MB (14.85%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int maximum69Number (int num) {
-        string s;
-        int rem=0;
-        int c=0;
-        string k=to_string(num);
-       for(int i=0;i<k.size();i++)
-       {
-          
-           if(k[i]=='6'&&c==0){
-           s+='9';c++;}
-           else
-           {
-           s+=k[i];}
-       }
-       return stoi(s);
+    int maximum69Number(int num) {
+        string s = to_string(num);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '6'){
+                s[i] = '9';
+                break;}
+            else
+                continue;
+        }
+        int a=stoi(s);
+        return a;
     }
 };
