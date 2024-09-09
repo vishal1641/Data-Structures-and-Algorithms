543b5c7667c6bba55e16b6f5bf9964654b11cecb
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-10 02:02:40 +0530

    Time: 9 ms (19.43%), Space: 13.1 MB (34.38%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    void solve(int i, vector<int>& nums, int target, vector<int>& ans,
-               vector<vector<int>> &result, int sum) {
-        if (sum > target)
-            return;
+    vector<vector<int>> ans;
+    void solve(int i, vector<int>& nums, int& sum, int& target,
+               vector<int>& temp) {
         if (sum == target) {
-            result.push_back(ans);
+            ans.push_back(temp);
             return;
         }
+        if (sum > target) {
+            return;
+        }
+
         for (int j = i; j < nums.size(); j++) {
             if(j>i&&nums[j]==nums[j-1])
             continue;
             sum += nums[j];
-            ans.push_back(nums[j]);
-            solve(j + 1, nums, target, ans, result, sum);
+            temp.push_back(nums[j]);
+            solve(j + 1, nums, sum, target, temp);
             sum -= nums[j];
-            ans.pop_back();
+            temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        vector<int> ans;
-        vector<vector<int>> result;
-        sort(begin(nums),end(nums));
 
+        vector<int> temp;
         int sum = 0;
-        solve(0, nums, target, ans, result, sum);
-        return result;
+         sort(begin(nums),end(nums));
+        solve(0, nums, sum, target, temp);
+       
+        return ans;
     }
 };
