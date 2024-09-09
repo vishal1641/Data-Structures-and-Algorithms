731d6ee01327405a89eead6299ee2f493aae8dae
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-09 18:26:10 +0530

    Time: 0 ms (100%), Space: 15 MB (34.25%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,39 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>>nodes;
-queue<pair<TreeNode*,pair<int,int>>>q;
-q.push({root,{0,0}});
-while(!q.empty()){
-    auto p=q.front();
-    q.pop();
-    TreeNode* node=p.first;
-   int x=p.second.first;
-    int y=p.second.second;
-    nodes[x][y].insert(node->val);
-    if(node->left!=NULL)
-    q.push({node->left,{x-1,y+1}});
-    if(node->right!=NULL)
-    q.push({node->right,{x+1,y+1}});
-}
-vector<vector<int>>ans;
-for(auto z:nodes){
-    vector<int>col;
-    for(auto r:z.second)
-    col.insert(col.end(),r.second.begin(),r.second.end());
-    ans.push_back(col);
-}
-return ans;
+
+        map<int, map<int, multiset<int>>> m;
+
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            TreeNode* node = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
+
+            m[x][y].insert(node->val);
+
+            if (node->left != NULL) {
+                q.push({node->left, {x - 1, y + 1}});
+            }
+            if (node->right != NULL) {
+                q.push({node->right, {x + 1, y + 1}});
+            }
+        }
+
+        vector<vector<int>> ans;
+        for (auto it : m) {
+            vector<int> col;
+            for (auto itt : it.second) {
+                col.insert(col.end(), itt.second.begin(), itt.second.end());
+            }
+            ans.push_back(col);
+        }
+
+        return ans;
     }
 };
