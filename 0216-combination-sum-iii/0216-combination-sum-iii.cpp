c3e26378197a8f6faaa14131775015c5d10eb68e
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-10 01:37:00 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.71%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    void solve(int i, int k, int n, vector<vector<int>>& ans,
-               vector<int>& temp,int sum) {
+    vector<vector<int>> ans;
+    void solve(int i,int k, int n, vector<int>& temp, int& sum) {
 
         if (k == 0) {
             if (sum == n)
                 ans.push_back(temp);
             return;
         }
-        if (sum >= n)
-            return;
-
         for (int j = i; j <= 9; j++) {
-            sum += j;
+            sum +=j;
+            k = k - 1;
             temp.push_back(j);
-            solve(j + 1, k - 1, n, ans,temp,sum);
+            solve(j + 1, k, n, temp, sum);
             sum -= j;
+            k = k + 1;
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
+
         vector<int> temp;
-        int sum=0;
-        solve(1, k, n, ans,temp,sum);
+        int sum = 0;
+        solve(1, k, n, temp, sum);
         return ans;
     }
 };
