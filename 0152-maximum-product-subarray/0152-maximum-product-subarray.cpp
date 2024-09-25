4507d0cbbff8e36938c69e8e6026e13e442da39a
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-26 01:14:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,40 +1,22 @@
-#include <algorithm>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-
-        if (n == 0)
-            return 0;
-
-        vector<int> a(n, 1);
-        vector<int> b(n, 1);
-
+        vector<int> a;
+        vector<int> b;
         int x = 1;
-        for (int i = 0; i < n; i++) {
-            x *= nums[i];
-            a[i] = x;
-            if (x == 0)
-                x = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            x = x * nums[i];
+            a.push_back(x);
         }
-
-        x = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            x *= nums[i];
-            b[i] = x;
-            if (x == 0)
-                x = 1;
+         x = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            x = x * nums[i];
+            b.push_back(x);
         }
-       
-        int maxi = nums[0];
-        for (int i = 0; i < n; i++) {
-           maxi=max({maxi,a[i],b[i]});
-            
+        int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+        maxi=max({maxi,b[i],a[i],nums[i]});
         }
-
         return maxi;
     }
 };
