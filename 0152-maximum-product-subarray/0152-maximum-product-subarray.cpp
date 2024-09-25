926ecd7129c347cdf989a20f5051bcd52b90479b
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-26 01:15:01 +0530

    Time: 7 ms (37.93%), Space: 17.7 MB (5.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -7,11 +7,15 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             x = x * nums[i];
             a.push_back(x);
+            if(x==0)
+            x=1;
         }
          x = 1;
         for (int i = nums.size() - 1; i >= 0; i--) {
             x = x * nums[i];
             b.push_back(x);
+            if(x==0)
+            x=1;
         }
         int maxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
