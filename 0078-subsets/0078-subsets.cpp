1ed84a782c92f9700f9acfec5f8d05102376edbb
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-09-27 23:50:29 +0530

    Time: 4 ms (39.3%), Space: 8.7 MB (53.21%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<vector<int>>& ans, int i, vector<int>& temp) {
-       
-            ans.push_back(temp);
-            
-        
-
+    vector<vector<int>> v;
+    void solve(vector<int>& nums, int i, vector<int>& temp) {
+        v.push_back(temp);
         for (int j = i; j < nums.size(); j++) {
-            temp.push_back(nums[j]);      // Include the current element
-            solve(nums, ans, j + 1, temp); // Recur with the next element
-            temp.pop_back();              // Backtrack to explore subsets without the current element
+            temp.push_back(nums[j]);
+            solve(nums, j + 1, temp);
+            temp.pop_back();
         }
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
         vector<int> temp;
-        solve(nums, ans, 0, temp);
-        return ans;
+        solve(nums, 0, temp);
+        return v;
     }
 };
