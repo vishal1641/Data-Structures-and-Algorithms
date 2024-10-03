bd1aa3cbed832400477ff29683201d92d8b6812e
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-10-03 23:02:30 +0530

    Time: 7 ms (57%), Space: 14.4 MB (95.69%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,35 +1,30 @@
-class myComparator {
+class Solution {
 public:
-    int operator()(pair<int,int>&p1,pair<int,int>&p2){
-        if(p1.first>p2.first)
+static bool comp(pair<int,int>a,pair<int,int>b){
+   if(a.first>b.first)
         return true;
-        else if(p1.first<p2.first)
+        else if(a.first<b.first)
         return false;
         else{
-            if(p1.second<p2.second)
+            if(a.second<b.second)
             return true;
         else return false;}
-    } };
-    class Solution {
-public:
+}
     vector<int> frequencySort(vector<int>& nums) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,myComparator>m;
-       unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+            m[nums[i]]++;
+        vector<pair<int, int>> p;
+        for (auto it : m) {
+            p.push_back({it.second, it.first});
         }
-         for(auto it:mp){
-            m.push({it.second,it.first});
-         
+        sort(begin(p), end(p), comp);
+        vector<int> v;
+        for (int i = 0; i < p.size(); i++) {
+            for (int j = 0; j < p[i].first; j++)
+                v.push_back(p[i].second);
         }
-        vector<int>v;
-        while(m.size()>0){
-
-            for(int i=1;i<=m.top().first;i++){
-                v.push_back(m.top().second);
-            }
-            m.pop();
-        }
+        reverse(begin(v),end(v));
         return v;
     }
 };
