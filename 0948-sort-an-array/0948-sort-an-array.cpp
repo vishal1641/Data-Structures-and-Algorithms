ce1e9b7d1914e7cea4fa8239295ca57b28a214df
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-10-26 19:18:11 +0530

    Time: 616 ms (20.08%), Space: 284.4 MB (11.94%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        priority_queue<int,vector<int>,greater<int>>m;
-        for(int i=0;i<nums.size();i++){
-            m.push(nums[i]);
-        }
-        vector<int>v;
-        while(m.size()>0){
-            v.push_back(m.top());
-            m.pop();
-        }
-        return v;
+    void merge(int s, int mid, int e, vector<int>& v) {
+        vector<int> ans;
+        int i = s; // Start from 's' for the left half
+        int j = mid + 1; // Start from 'mid + 1' for the right half
 
+        while (i <= mid && j <= e) {
+            if (v[i] <= v[j]) {
+                ans.push_back(v[i]);
+                i++;
+            } else {
+                ans.push_back(v[j]);
+                j++;
+            }
+        }
+        while (i <= mid) {
+            ans.push_back(v[i]);
+            i++;
+        }
+        while (j <= e) {
+            ans.push_back(v[j]);
+            j++;
+        }
+        for (int k = 0; k < ans.size(); k++) {
+            v[s + k] = ans[k]; 
+        }
+    }
+
+    void solve(int s, int e, vector<int>& v) {
+        if (s >= e)
+            return;
+        int mid = s + (e - s) / 2;
+        solve(s, mid, v);
+        solve(mid + 1, e, v);
+        merge(s, mid, e, v);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int e = nums.size() - 1;
+        solve(0, e, nums);
+        return nums;
     }
 };
