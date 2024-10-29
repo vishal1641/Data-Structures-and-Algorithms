a62b61b9abd61fe46531ae5aca096808ab58891d
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-10-29 17:23:07 +0530

    Time: 0 ms (100%), Space: 12.8 MB (45.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    void solve(vector<int>& nums, int target, int i, int sum, vector<int>& temp) {
-        if (sum > target)
-            return;
-        if (sum == target) {
-            v.push_back(temp);
-            return;
-        }
-        if (i >= nums.size())
-            return;
-        for (int j = i; j < nums.size(); j++) {
-            sum += nums[j];
-            temp.push_back(nums[j]);
-            solve(nums, target, j, sum, temp);
-            sum -= nums[j];
-            temp.pop_back();
-        }
+vector<vector<int>>ans;
+void solve(int i,vector<int>&temp,vector<vector<int>>&ans,int sum,int target,vector<int>&nums){
+if(sum>target)
+return;
+if(sum==target)
+ans.push_back(temp);
+if(i>=nums.size())
+return ;
+
+    for(int j=i;j<nums.size();j++){
+        sum+=nums[j];
+        temp.push_back(nums[j]);
+        solve(j,temp,ans,sum,target,nums);
+        sum-=nums[j];
+        temp.pop_back();
     }
+}
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        int i = 0;
-        vector<int> temp;
-        solve(nums, target, i, 0, temp);
-        return v;
+        vector<int>temp;
+        solve(0,temp,ans,0,target,nums);
+        return ans;
     }
 };
