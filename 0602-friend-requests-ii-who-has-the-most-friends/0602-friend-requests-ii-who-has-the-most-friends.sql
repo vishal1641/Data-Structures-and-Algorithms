8b17ce8b330490080390ce3a99c995a2d47d6580
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-11-03 16:46:24 +0530

    Time: 339 ms (57.53%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,11 @@
+SELECT id, COUNT(*) AS num 
+FROM (
+    SELECT requester_id AS id 
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id 
+    FROM RequestAccepted
+) request
+GROUP BY id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
