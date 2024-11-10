704ba4ee32bee7316ccc52e6f2cc6447c165e0ad
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2024-11-10 13:44:34 +0530

    Time: 0 ms (100%), Space: 17.7 MB (79.85%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -6,8 +6,7 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -15,24 +14,10 @@ public:
     int maxDepth(TreeNode* root) {
         if(root==NULL)
         return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int count=0;
-        while (q.size() > 0) {
-            int n = q.size();
-            vector<int> a;
-            while (n--) {
-                TreeNode* temp = q.front();
-                q.pop();
-                a.push_back(temp->val);
-                if (temp->left != NULL)
-                    q.push(temp->left);
-                if (temp->right != NULL)
-                    q.push(temp->right);
-            }
-            if (a.size() > 0)
-                count++;
-        }
-        return count;
+
+
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+        return 1+max(lh,rh);
     }
 };
