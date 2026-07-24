005a744e203bce88875175d88a626c855250fe40
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2026-07-24 20:05:15 +0530

    Time: 0 ms (100%), Space: 10.7 MB (40.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-vector<vector<int>>ans;
-void solve(int i,vector<int>&temp,vector<int>&nums){
-    ans.push_back(temp);
-    for(int j=i;j<nums.size();j++){
-        if(j>i&&nums[j]==nums[j-1])
-        continue;
-        temp.push_back(nums[j]);
-        solve(j+1,temp,nums);
-        temp.pop_back();
+    vector<vector<int>> v;
+    void solve(vector<int>& nums, vector<int>& temp, int i, int sum) {
+        if (i >= nums.size())
+            return;
+
+        for (int j = i; j < nums.size(); j++) {
+            if (j > i && nums[j - 1] == nums[j])
+                continue;
+            temp.push_back(nums[j]);
+            v.push_back(temp);
+            solve(nums, temp, j + 1, sum);
+
+            temp.pop_back();
+        }
     }
-}
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int>temp;
+        int sum = 0;
         sort(begin(nums),end(nums));
-        solve(0,temp,nums);
-        return ans;
+        vector<int>temp;
+        v.push_back({});
+        solve(nums, temp, 0, 0);
+        return v;
     }
 };
