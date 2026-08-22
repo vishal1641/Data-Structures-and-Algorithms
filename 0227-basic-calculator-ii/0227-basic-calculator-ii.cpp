5e65b5ce0271c1093265f04f32df3ede43ef2899
Vishal Kumar Tiwari <114956239+vishal1641@users.noreply.github.com>
2026-08-22 21:01:15 +0530

    Time: 7 ms (63.57%), Space: 15.1 MB (72.26%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,37 +1,61 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int>st;
-      char  sign='+';
-       int c=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(isdigit(s[i]))
-            c=c*10+(s[i]-'0');
-            if(!isdigit(s[i])&&s[i]!=' '||i==s.size()-1)
-            {
-                if(sign=='+')
-                st.push(c);
-                else if(sign=='-')
-                st.push(-1*c);
-                else{
-                    if(sign=='*'){
-                        int num=st.top();
-                        st.pop();
-                    st.push(num*c);}
-                    else{
-                    int num=st.top();
-                        st.pop();
-                    st.push(num/c);}
+
+        stack<int> st;
+
+        int number = 0;
+        char sign = '+';
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (isdigit(s[i])) {
+
+                number = number * 10 + (s[i] - '0');
+            }
+
+            if ((!isdigit(s[i]) && s[i] != ' ') || i == s.size() - 1) {
+
+                if (sign == '+') {
+
+                    st.push(number);
                 }
-                sign=s[i];
-                c=0;
+
+                else if (sign == '-') {
+
+                    st.push(-number);
+                }
+
+                else if (sign == '*') {
+
+                    int a = st.top();
+                    st.pop();
+
+                    st.push(a * number);
+                }
+
+                else if (sign == '/') {
+
+                    int a = st.top();
+                    st.pop();
+
+                    st.push(a / number);
+                }
+
+                number = 0;
+
+                sign = s[i];
             }
         }
-        int sum=0;
-        while(!st.empty()){
-            sum+=st.top();st.pop();
+
+        int result = 0;
+
+        while (!st.empty()) {
+
+            result += st.top();
+            st.pop();
         }
-        return sum;
+
+        return result;
     }
 };
